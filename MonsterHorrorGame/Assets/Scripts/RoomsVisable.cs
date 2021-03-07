27dbbefa5fe1871c95b5cdb0b292f157e151df90
Ark <theonlypurplecat@gmail.com>
2021-03-07 22:55:06 +1300

    A countdown script

---
+++ MonsterHorrorGame/Assets/Scripts/RoomsVisable.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomsVisable : MonoBehaviour
+{
+//put a room's walls in one empty GameObject,
+//and the interior stuff in a secondary one under that
+
+  [SerializeField]
+  private GameObject roomWalls = null;
+
+  [SerializeField]
+  private GameObject roomInterior = null;
+
+  //has this room been into before?
+  [SerializeField]
+  private bool investigated = false;
+
+  private bool insideRoom = false;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+
+private void OnOff()
+{
+  if (investigated == false)
+  {
+    RoomEnterFirst();
+  }
+  else
+  {
+    if (insideRoom == false)
+    {
+      RoomEnter();
+    }
+    else
+    {
+      RoomExit();
+    }
+
+  }
+}
+
+
+private void RoomEnterFirst()
+{
+  roomWalls.SetActive(true);
+  roomInterior.SetActive(true);
+}
+
+private void RoomEnter()
+{
+  roomInterior.SetActive(true);
+}
+
+private void RoomExit()
+{
+  roomInterior.SetActive(false);
+}
+
+
+
+
+}
---
+++ MonsterHorrorGame/Assets/Scripts/countdown.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class countdown : MonoBehaviour
+{
+//the script to run counting for spawning in the monster periodicaly
+
+  private int seconds = 0;
+  private int countTime = 20;
+
+  private bool monsterInPlay = false;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void StartCount()
+    {
+      StartCoroutine(CountingSeconds());
+
+    }
+
+
+    IEnumerator CountingSeconds()
+    {
+      if (seconds > 0)
+      {
+        //double check this code here:
+        yield return new WaitForSeconds(1);
+        seconds --;
+
+      }
+      else if (monsterInPlay == false)
+      {
+        //spawn monster script thing goes here.
+       monsterInPlay = true;
+      }
+
+
+
+    }
+
+}
