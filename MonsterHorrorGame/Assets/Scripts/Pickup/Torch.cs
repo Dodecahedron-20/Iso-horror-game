96cf92c808349faf37b87b4bb8424282cdbbce93
Al <theonlypurplecat@gmail.com>
2021-03-16 12:43:57 +1300

    collecting torch

---
+++ MonsterHorrorGame/Assets/Scripts/Pickup/Torch.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Torch : MonoBehaviour
+{
+
+
+    //torch item:
+    [SerializeField]
+    private GameObject torch = null;
+
+    //light on player:
+    [SerializeField]
+    private GameObject playerLight = null;
+
+    //UI Icon
+    [SerializeField]
+    private GameObject torchIcon = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerLight.SetActive(false);
+        torchIcon.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        torchIcon.SetActive(true);
+        playerLight.SetActive(true);
+        Destroy(torch);
+    }
+
+
+
+
+}
