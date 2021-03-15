c42fc6eae511be2a68ca956adaae831ea1c71f9e
Al <theonlypurplecat@gmail.com>
2021-03-15 14:58:20 +1300

    some sorting, and key script added

---
+++ MonsterHorrorGame/Assets/Scripts/Pickup/FinalKey.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinalKey : MonoBehaviour
+{
+
+    //Key itself
+    [SerializeField]
+    private GameObject key = null;
+
+    //the final door
+    // [SerializeField]
+    //private (something) endDoor;
+
+    [SerializeField]
+    private GameObject keyIcon = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        keyIcon.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        CollectKey();
+    }
+
+    private void CollectKey()
+    {
+        keyIcon.SetActive(true);
+        Destroy(key);
+    }
+}
