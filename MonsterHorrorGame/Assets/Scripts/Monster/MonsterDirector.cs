032d96532e672b6f4afdba06f8c3dd10cbdd0fac
Jet Yong <jettlag2000@gmail.com>
2021-03-20 20:40:20 +1300

    MonsterDirectorWIP

--- MonsterHorrorGame/Assets/Scripts/Monster/MonsterDirector.cs
+++ MonsterHorrorGame/Assets/Scripts/Monster/MonsterDirector.cs
@@ -6,12 +6,31 @@ public class MonsterDirector : MonoBehaviour
 {
     public Monster monster;
 
+    [SerializeField] bool monsterActive;
     [SerializeField] float startTimer;
     [SerializeField] float timeSinceLastSpawn;
     [SerializeField] float playerDistance;
+    
+    public List<Waypoint> spawnPoints;
+
+    void Start()
+    {
+        monsterActive = false;
+
+        StartCoroutine(CheckMonsterStatus());
+    }
 
     void Update()
     {
 
+    }
+
+    IEnumerator CheckMonsterStatus()
+    {
+        yield return new WaitForSeconds(5f);
+        if(startTimer <= 0 && timeSinceLastSpawn > 5)
+        {
+            monsterActive = true;
+        }
     }
 }
