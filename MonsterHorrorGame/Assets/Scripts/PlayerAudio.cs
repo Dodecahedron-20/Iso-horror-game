4103262876e03e9f9beef042f888f4a0607294eb
Ark <theonlypurplecat@gmail.com>
2021-03-22 21:27:01 +1300

    some more walls and the beginning of sound

---
+++ MonsterHorrorGame/Assets/Scripts/PlayerAudio.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAudio : MonoBehaviour
+{
+  //[SerializeField]
+  //private AudioSource step;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
--- MonsterHorrorGame/Assets/Scripts/enviroments/DoorOpen.cs
+++ MonsterHorrorGame/Assets/Scripts/enviroments/DoorOpen.cs
@@ -10,17 +10,26 @@ public class DoorOpen : MonoBehaviour
     [SerializeField]
     private bool unlocked = true;
 
+    private bool beep = false;
+
+    [SerializeField]
+    private AudioSource openAudio = null;
+    [SerializeField]
+    private AudioSource closeAudio = null;
+    [SerializeField]
+    private AudioSource beepAudio = null;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     private void OnTriggerEnter(Collider other)
@@ -29,14 +38,34 @@ public class DoorOpen : MonoBehaviour
         {
             anim.SetTrigger("opendoor");
         }
-        
+
 
     }
 
     public void UnLock()
     {
         unlocked = true;
+        beep = true;
     }
+
+
+//audio goes here:
+    public void OpenAudio()
+    {
+      if (beep == true)
+      {
+        beepAudio.Play();
+      }
+
+      openAudio.Play();
+
+    }
+
+    public void CloseAudio()
+    {
+      closeAudio.Play();
+    }
+
 
 
 
