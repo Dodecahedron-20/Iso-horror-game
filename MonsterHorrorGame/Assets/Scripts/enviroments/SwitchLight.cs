bcdbe3d232dfbbf0ad7a9e395123324d31d18500
Jet Yong <jettlag2000@gmail.com>
2021-03-25 15:26:20 +1300

    thursdayedits

--- MonsterHorrorGame/Assets/Scripts/enviroments/SwitchLight.cs
+++ MonsterHorrorGame/Assets/Scripts/enviroments/SwitchLight.cs
@@ -2,22 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerAudio : MonoBehaviour
+public class SwitchLight : MonoBehaviour
 {
-  //[SerializeField]
-  //private AudioSource step;
-
-
-
     // Start is called before the first frame update
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        
     }
 }
