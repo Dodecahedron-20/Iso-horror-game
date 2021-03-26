a36c5c50728e6b1baad19464286e7296216e5e1c
Jet Yong <jettlag2000@gmail.com>
2021-03-26 15:05:36 +1300

    monsterprefab

---
+++ MonsterHorrorGame/Assets/Scripts/Player/KillPlayer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillPlayer : MonoBehaviour
+{
+    public Player player;
+    public Monster monster;
+    public float damage;
+
+    void OnTriggerEnter(Collider player)
+    {
+        if (player.gameObject.tag == "Player")
+        {
+            Debug.Log("pow");
+            StartCoroutine(Kill());           
+        }
+    }
+
+    IEnumerator Kill()
+    {
+        yield return new WaitForSeconds(0.1f);
+        player = GetComponent<Player>();
+        monster = GetComponent<Monster>();
+        player.moveSpeed = 0f;
+        monster.nav.speed = 0f;
+
+        player.gameObject.GetComponent<Player>().health -= damage;
+    }
+
+}
