5e95492afa89d6639f0985a40932d98cbc2ca831
Al <theonlypurplecat@gmail.com>
2021-03-27 19:44:09 +1300

    some additions and things

---
+++ MonsterHorrorGame/Assets/Scripts/Monster/beginMonster.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class beginMonster : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject monCtrl = null;
+    [SerializeField]
+    private GameObject item = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        monCtrl.SetActive(true);
+        Destroy(item);
+    }
+
+}
