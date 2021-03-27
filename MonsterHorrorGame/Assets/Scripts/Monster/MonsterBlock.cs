e27ad0e74d2a478ab913b397e431fa33b6567bc1
Ark <theonlypurplecat@gmail.com>
2021-03-28 00:56:57 +1300

    replacing the vaneshing script

---
+++ MonsterHorrorGame/Assets/Scripts/Monster/MonsterBlock.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterBlock : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject thisthing = null;
+    [SerializeField]
+    private GameObject monDirect = null;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+      monDirect.SetActive(true);
+      Destroy(thisthing);
+    }
+}
